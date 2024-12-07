e4a6b279c536e9e474065a38e3d264c2c7ddc206
ctaepais <ctaepais@andrew.cmu.edu>
2024-12-07 17:12:26 -0500

    updated US9

--- scripts/us9-average_review_rating-complex-analytical.py
+++ scripts/us9-average_review_rating-complex-analytical.py
@@ -1,38 +1,37 @@
 from common import *
 
-def average_review_rating(providerID):
+def total_travel_services(providerID):
     
     us = '''
-    US9: View Average Review Rating
+    US9: View Total Available Service Listings
     Type: Complex + Analytical
 
     As a:  Service Provider
-    I want:  To view the average review rating of my services
-    So That:  I can improve offerings to attract more customers
+    I want:  To view the total number of available travel service listings
+    So That:  I can manage offerings to attract more customers
     
-    Case: View the average review rating of Marriot
-    Function Call: average_review_rating(4)
+    Case: View the total number of available travel listings of Marriot
+    Function Call: total_travel_services(4)
     '''
     
     print(us)
     
-    cols = "name, type, reviewrating"
+    cols = "name, type, totallistings"
     
     query = '''
-        SELECT s.name, s.type, ROUND(AVG(r.rating), 2)
+        SELECT s.name, s.type, COUNT(listingid)
           FROM Service_Provider AS s 
                JOIN Travel_Service AS t ON s.providerid = t.providerid
-               JOIN Review AS r ON t.listingid = r.listingid
-         WHERE s.providerid = %s
+         WHERE s.providerid = %s AND t.available = TRUE
          GROUP BY s.name, s.type
     '''
     
     cmd = cur.mogrify(query, (providerID,))
     cur.execute(cmd)
     rows = cur.fetchall()
-    print("Average Review Rating")
+    print("Total Travel Listings")
     show_table(rows, cols)
     
     return
 
-average_review_rating(4)
+total_travel_services(4)
